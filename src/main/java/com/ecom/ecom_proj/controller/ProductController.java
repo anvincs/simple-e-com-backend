7a79bb99827386d4c7893fc228385cac164a2283
Anvin.C.Shaju <anvincsg@gmail.com>
2024-07-11 10:11:12 +0530

    Added ResponseEntity to send status codes

--- src/main/java/com/ecom/ecom_proj/controller/ProductController.java
+++ src/main/java/com/ecom/ecom_proj/controller/ProductController.java
@@ -3,26 +3,35 @@ package com.ecom.ecom_proj.controller;
 import com.ecom.ecom_proj.model.Product;
 import com.ecom.ecom_proj.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.http.HttpResponse;
 import java.util.List;
 
 @RestController
+@CrossOrigin // to prevent cors error
 @RequestMapping("/api") // base url. every mapping in the class methods will have a /api before it
 public class ProductController {
 
     @Autowired
     private ProductService service;
 
-    @RequestMapping("/") // this is /api
-    public String greet() {
-        return "Hello World !";
+    @GetMapping("/products")
+    // ResponseEntity is required to send the status along with the data
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = service.getAllProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
-    @GetMapping("/products")
-    public List<Product> getAllProducts() {
-        return service.getAllProducts();
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable int id) {
+        Product product = service.getProductById(id);
+        if(product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
     }
 }
